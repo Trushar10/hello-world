164d27f8f707f7b75dfad8a76dd175162bee4aa6
Trushar10 <63665542+Trushar10@users.noreply.github.com>
2021-05-18 21:58:41 +0530

    LOgo

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import React from "react"
 import { Container } from "../components"
 
 
-export default function Home() {
+const IndexPage = () => {
   return (
     <Container> 
       <h1>Hello World</h1>
     </Container>
   )
 }
+
+export default IndexPage
