17c8b1be7412b3c6a08a13610f6841cd92044fa1
Trushar10 <63665542+Trushar10@users.noreply.github.com>
2021-05-19 19:17:03 +0530

    Card added

--- src/elements/ContentElements.js
+++ src/elements/ContentElements.js
@@ -4,7 +4,7 @@ export const ContentWrapper = styled.main`
     grid-column:4 / span 8;
     grid-row: 3 / span 5;
     background-color: ${props => props.theme.colors.light2};
-    padding: ${props => `${props.theme.spacing.xLarge} ${props.theme.spacing.xxLarge}`};
+    padding: ${props => `${props.theme.spacings.xLarge} ${props.theme.spacings.xxLarge}`};
     box-shadow: ${props => props.theme.shadows.shadow1};
     z-index: 10;
 
@@ -13,6 +13,6 @@ export const ContentWrapper = styled.main`
     }
 
     @media ${props => props.theme.breakpoints.mobile}{
-        padding: ${props => `${props.theme.spacing.medium} ${props.theme.spacing.large}`};
+        padding: ${props => `${props.theme.spacings.medium} ${props.theme.spacings.large}`};
     }
 `
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import React from "react"
-import { Container, FeatureImage } from "../components"
+import { Container, FeatureImage, Content, ContentCard} from "../components"
 
 
 const IndexPage = () => {
   return (
     <Container> 
       <FeatureImage></FeatureImage>
+      <Content>
+        <ContentCard date="March 24, 2020" title="Lorem ipsum dolor sit amet" excerpt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit nisl, leo et. Vitae hendrerit vivamus cum ullamcorper hendrerit et arcu, elit ornare." slug="/test">
+      </ContentCard>
+      </Content>
     </Container>
   )
 }
