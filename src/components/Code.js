85ec4dfd59091b7ef025558a20173f7c262c7dd6
Trushar10 <63665542+Trushar10@users.noreply.github.com>
2021-05-23 18:52:22 +0530

    update

--- src/components/Code.js
+++ src/components/Code.js
@@ -2,8 +2,7 @@ import React from "react"
 import Highlight, {defaultProps} from "prism-react-renderer"
 import theme from "prism-react-renderer/themes/vsDark"
 
-export const Code = ({codeString, language, ...props}) => 
-{
+export const Code = ({codeString, language, ...props}) => {
     return (
         <Highlight
         {...defaultProps}
