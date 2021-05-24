d52ba4c24cb9ec77b32ade4fb96856ee26c34917
Trushar10 <63665542+Trushar10@users.noreply.github.com>
2021-05-24 16:00:26 +0530

    commit

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,31 +1,31 @@
 import React from "react"
-import {graphql} from "gatsby"
-import {Container, Content, FeatureImage} from "../components"
-import {H1} from "../elements"
+import { graphql } from "gatsby"
+import { Container, Content, FeatureImage } from "../components"
+import { H1 } from "../elements"
 
-const notFound =({data}) => {
-    const featureImage = data.imageSharp.fixed 
-    
-    return(
-        <Container>
-            <FeatureImage fixed={featureImage}>
-                <H1 textAlign="center" margin="0 0 1rem 0">
-                    {" "}
-                    Uh-oh... What you're looking for couldn't be found
-                </H1>
-            </FeatureImage>
-        </Container>
-    )
+const notFound = ({ data }) => {
+  const featureImage = data.imageSharp.fixed
+
+  return (
+    <Container>
+      <FeatureImage fixed={featureImage}>
+        <H1 textAlign="center" margin="0 0 1rem 0">
+          {" "}
+          Uh-oh... What you're looking for couldn't be found
+        </H1>
+      </FeatureImage>
+    </Container>
+  )
 }
 
 export default notFound
 
 export const notFoundQuery = graphql`
-    query NotFoundQuery {
-        imageSharp (fixed: { originalName: { eq: "sad-cat.jpg"}}){
-            fixed{
-                ...GatsbyImageSharpFixed
-            }
-        }
+  query NotFoundQuery {
+    imageSharp(fixed: { originalName: { eq: "sad-cat.jpg" } }) {
+      fixed {
+        ...GatsbyImageSharpFixed
+      }
     }
+  }
 `
